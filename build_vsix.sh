af095813e2475c02bbec150ea63d034790065dd0
mar3hc <tri.maiminh@vn.bosch.com>
2025-10-24 14:18:39 +0700

    update failed message

--- build_vsix.sh
+++ build_vsix.sh
@@ -2,6 +2,6 @@
 
 # Run vsce to package or publish the extension
 echo "Checking if vsce is installed..."
-command -v vsce || { echo "Error: vsce is not installed. Please install it first." >&2; exit 1; }
+command -v vsce || { echo "Error: vsce is not installed. Please install it with: npm install -g @vscode/vsce" >&2; exit 1; }
 echo "Running vsce..."
 vsce package --allow-missing-repository || { echo "Error: Failed to package the extension using vsce" >&2; exit 1; }
