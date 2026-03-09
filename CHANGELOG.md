4d6ce9ccec99c05ca4758de10aa189672057d09f
Mai Minh Tri (MS/EMC51-XC) <Tri.MaiMinh@vn.bosch.com>
2026-03-09 14:51:54 +0700

    remove redundant file to install destination

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,11 @@ Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how 
 ## [0.0.1] - 2025-06-30
 
 - Initial release
+
+## [0.0.2] - 2025-11-30
+
+- Redesigned the welcome page with a new look.
+- Added functionality to load URLs or local HTML files into the webview.
+- Added features to open files or folders from the welcome page.
+- Included library documentation for RobotFramework AIO keywords.
+- Added a "Welcome" button to the status bar (bottom-right), enabling quick and seamless access to the redesigned welcome page at any time.
--- build_vsix.sh
+++ build_vsix.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+echo "Run genpackagedoc"
+"${RobotPythonPath}/python" genpackagedoc.py
+
 # Run vsce to package or publish the extension
 echo "Checking if vsce is installed..."
 command -v vsce || { echo "Error: vsce is not installed. Please install it with: npm install -g @vscode/vsce" >&2; exit 1; }
 echo "Running vsce..."
-vsce package --allow-missing-repository || { echo "Error: Failed to package the extension using vsce" >&2; exit 1; }
+vsce package --allow-missing-repository --out ./VSCodeWelcome || { echo "Error: Failed to package the extension using vsce" >&2; exit 1; }
